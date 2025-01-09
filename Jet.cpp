b4cab33a08568fb8e6cee09628c76f9f08e94f7c
JetriiRevasonGerald <jetriigerald69@gmail.com>
2025-01-09 21:01:15 +0300

    students system
    allows users to enter, show, search, update and delete students information.

--- Jet.cpp
+++ Jet.cpp
@@ -72,7 +72,7 @@ void search(){
 		}
 	}
  }
-void update(){
+void update();{
 	if(total=0){
 		cout<<"No data is entered"<<endl;
 	}else{
@@ -103,7 +103,7 @@ void update(){
 	}
   }
 } 
-void deleterecord(){
+void deleterecord();{
 	if(total=0){
 		cout<<"No data is entered"<<endl;
 	}else{
@@ -136,3 +136,42 @@ void deleterecord(){
 	
  }
 }
+main()
+{
+	int value;
+	while(true)
+	{
+	cout<<"Press 1 to enter data"<<endl;
+	cout<<"Press 2 to show data"<<endl;
+	cout<<"Press 3 to search data"<<endl;
+	cout<<"Press 4 to update data"<<endl;
+	cout<<"Press 5 to delete data"<<endl;
+	cout<<"Press 6 to exit"<<endl;
+	cin>>value;
+	switch(value)
+	{
+		case 1:
+			enter();
+			break;
+				case 2:
+			show();
+			break;
+				case 3:
+			search();
+			break;
+				case 4:
+			update();
+			break;
+				case 5:
+			deleterecord();
+			break;
+				case 6:
+			exit(0);
+			break;
+				default:
+			cout<<"Invalid input"<<endl;
+			break;
+	}
+	}
+}
+
